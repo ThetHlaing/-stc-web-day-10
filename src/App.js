110045149b83fb4da21b8fbad08833010f17250f
Thet Hlaing <th.ucsy@gmail.com>
2018-11-24 14:38:11 +0630

    Update the initial stage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import LoginCount from './recap/LoginCount';
-import ControlledComponent from './controlled_component/index-final';
+import ControlledComponent from './controlled_component/index';
 import NameForm from './uncontrolled_component/index';
-import Calculator from './currency_changer/index-final';
+import Calculator from './currency_changer/index';
 import BasicExample from './router/index';
 
 class App extends Component {
   render() {
     return (
       <div>      
-        <BasicExample/>
+        <LoginCount/>
       </div>
     );
   }
